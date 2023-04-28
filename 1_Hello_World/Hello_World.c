27f789f3aa1171aa4f1e4dc259daffe8077b7c28
SinusFox <sinusfox@pm.me>
2023-04-28 09:22:02 +0200

    Added line break

--- 1_Hello_World/Hello_World.c
+++ 1_Hello_World/Hello_World.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Printing hello world, although printing foxes is superior.");
+    printf("Printing hello world,\nalthough printing foxes is superior.");
     return 0;
 }
