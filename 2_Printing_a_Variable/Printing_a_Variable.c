83b9e0b48f09a962404ffc41c0411dd642cedc5a
Damon Leven <dleven@protonmail.com>
2023-04-28 17:49:10 +0000

    Update Printing_a_Variable.c
    Actually printed fox :P

--- 2_Printing_a_Variable/Printing_a_Variable.c
+++ 2_Printing_a_Variable/Printing_a_Variable.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int j = 1;
-    printf("Printing %i,\nalthough printing foxes is superior.", j);
-    return 0;
+        int i = 3;
+        const char fox[4] = "FOX";
+
+        printf("Printing ");
+        for (unsigned int j = 0; j < 3; j++) {
+                printf("%c", fox[j]);
+        }
+
+        printf(", Since they are better :P\n");
+
+        return 0;
 }
