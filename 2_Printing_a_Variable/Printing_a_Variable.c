c69e3ca5c51a43aef2e9e06ddf67353d59e88a84
Damon Leven <dleven@protonmail.com>
2023-04-28 17:54:19 +0000

    Optimized printing
    FOXFOXFOXFOXFOXFOXFOXFOXFOXFOXFOX

--- 2_Printing_a_Variable/Printing_a_Variable.c
+++ 2_Printing_a_Variable/Printing_a_Variable.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main() {
-        int i = 3;
-        const char fox[4] = "FOX";
+        int i = 70;
 
         printf("Printing ");
-        for (unsigned int j = 0; j < 3; j++) {
-                printf("%c", fox[j]);
+        for (unsigned int j = i; j < 89; j += 9) {
+                printf("%c", j);
         }
 
         printf(", Since they are better :P\n");
